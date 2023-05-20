8b3b7085cc1de9bf725ac47c377e86d9d8f0ecdb
Seongmuk <creativeapple@naver.com>
2023-05-21 01:49:57 +0900

    Activate EEPROM

--- DIY_mini_drone/DIY_mini_drone.ino
+++ DIY_mini_drone/DIY_mini_drone.ino
@@ -7,42 +7,54 @@
 #include "RxModule.h"
 #include "JoyStick.h"
 #include "PWM.h"
+#include "EEPROM_USER.h"
 
-void setup() {
+float p = 1.11;
+float i = 2.22;
+float d = 3.33;
+
+void setup()
+{
   /* Init Periperal */
   Wire.begin();                 // I2C Master mode init
   Serial.begin(115200);         // UART init
   Serial.println("hello world");
 
-  Timer2_Init();
+//  Timer2_Init();
   
   /* Init Module */
-  MPU6050_INIT();
-  RF24_Rx_Init();
+//  MPU6050_INIT();
+//  RF24_Rx_Init();
 
-  attachInterrupt(digitalPinToInterrupt(2), RF24_Rx_Read, FALLING);
-  PWM_Init();
+//  attachInterrupt(digitalPinToInterrupt(2), RF24_Rx_Read, FALLING);
+//  PWM_Init();
+
+
+    EP_PID_Write(0,5.5,3.3,888.8);
+    Serial.println((String)p+"\t"+i+"\t"+d+"\t");
+    float gain[3];
+    EP_PID_Read(0,&gain[0],&gain[1],&gain[2]);
+    Serial.println((String)gain[0]+"\t"+gain[1]+"\t"+gain[2]+"\t");
+
 }
 
-void loop() {
-  if(Timer2_2ms_flag == 1)
-  {
-    Timer2_2ms_flag = 0;
+void loop()
+{
+//  if(Timer2_2ms_flag == 1)
+//  {
+//    Timer2_2ms_flag = 0;
 //    MPU6050_Read_Data(&mpu6050);
 //    MPU6050_Convert_Data(&mpu6050);
 //    CalculateCompliFilter(&Angle, &mpu6050);
 //    print_3_float(Angle.ComFilt_roll, Angle.ComFilt_pitch, Angle.ComFilt_yaw);
-
-    OCR0A = LV/4;
-    OCR0B = LH/4;
-    OCR1A = RV/4;
-    OCR1B = RH/4;
+//
+//    OCR0A = LV/4;
+//    OCR0B = LH/4;
+//    OCR1A = RV/4;
+//    OCR1B = RH/4;
 
     
-  }
-//  Serial.print(TIMSK0,HEX); Serial.print("\t"); Serial.println(TIFR0,HEX);
-//  Serial.print(TIMSK0,BIN); Serial.print("\t"); Serial.println(TIFR0,BIN);
-//  delay(100);
+//  }
 
   
 }
